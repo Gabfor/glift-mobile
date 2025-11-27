de80cea94935ec041ac3e4c5a2f09940ed1ede0d
Gabfor <68785462+Gabfor@users.noreply.github.com>
2025-11-27 22:07:08 +0100

    Tint effort cells with effort state colors

--- lib/training_details_page.dart
+++ lib/training_details_page.dart
@@ -281,7 +281,7 @@ class _ExerciseCardState extends State<_ExerciseCard> {
                       child: Container(
                         height: 40,
                         decoration: BoxDecoration(
-                          color: Colors.white,
+                          color: backgroundColor,
                           borderRadius: BorderRadius.circular(8),
                           border: Border.all(color: const Color(0xFFECE9F1)),
                         ),
