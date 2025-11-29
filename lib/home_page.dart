9bb975e094819886407430b98ed405fae89136ae
Gabfor <68785462+Gabfor@users.noreply.github.com>
2025-11-29 14:08:17 +0100

    Adjust training transitions

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -221,11 +221,25 @@ class _HomePageState extends State<HomePage> {
               training: training,
               onTap: () {
                 Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => TrainingDetailsPage(
+                  PageRouteBuilder(
+                    pageBuilder: (_, __, ___) => TrainingDetailsPage(
                       training: training,
                       supabase: widget.supabase,
                     ),
+                    transitionsBuilder:
+                        (_, animation, secondaryAnimation, child) {
+                      const begin = Offset(0, 1);
+                      const end = Offset.zero;
+                      const curve = Curves.ease;
+
+                      final tween = Tween(begin: begin, end: end)
+                          .chain(CurveTween(curve: curve));
+
+                      return SlideTransition(
+                        position: animation.drive(tween),
+                        child: child,
+                      );
+                    },
                   ),
                 );
               },
